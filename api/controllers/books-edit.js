6c1751878f53b0ffceff792b76d5e0989b25f675
Andrey Brunetkin <brunetkin@reg.ru>
2018-08-20 20:01:46 +0500

    мелкие правки

--- api/controllers/books-edit.js
+++ api/controllers/books-edit.js
@@ -4,17 +4,7 @@ const Book = require('../models/book');
 class BooksEdit {
 
     static async updateBook(ctx) {
-        let book = JSON.parse(ctx.request.body);
-
-        /* {
-            id          : 1,
-            title       : 'my new title',
-            date        : '2018-04-20',
-            author      : 'Klim Malkovich',
-            description : 'new descript',
-            image       : 'this is image!',
-        }; */
-
+        const book = ctx.request.body;
         const [result] = await Book.editItem(book);
 
         ctx.body = result;
